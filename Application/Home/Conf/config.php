c781045da5ed2e22f4762ace84f999713fbe8581
NeddiePeng <576970513@qq.com>
2017-07-26 07:53:13 +0800

    增加:后台管理登录注册模板

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING' => array(
+        '__CSS__' => __ROOT__.'/Public/resource/css',
+        '__JS__' => __ROOT__.'/Public/resource/script',
+        '__IMG__' => __ROOT__.'/Public/resource/images',
+    ),
 );
